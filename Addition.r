793f292af59bebf06e654395167940c073474d94
21CSE012 Yogin K <113223779+21CSE012-Yogin@users.noreply.github.com>
2023-05-04 20:58:38 +0530

    Update and rename Addtition to Addition.r

--- Addition.r
+++ Addition.r
@@ -1,13 +1,13 @@
 CODE :
 
-number_One <- as.integer(readline("Enter the first number : "))
+number_One <- as.integer(readline("Enter the first number  : "))
 number_Two <- as.integer(readline("Enter the second number : "))
 print(paste("Addition of",number_One, "and",number_Two, "is :",number_One + number_Two))
 
 OUTPUT :
 
-> number_One <- as.integer(readline("Enter the first number : "))
-Enter the first number : 11
+> number_One <- as.integer(readline("Enter the first number  : "))
+Enter the first number  : 11
 > number_Two <- as.integer(readline("Enter the second number : "))
 Enter the second number : 03
 > print(paste("Addition of",number_One, "and",number_Two, "is :",number_One + number_Two))
